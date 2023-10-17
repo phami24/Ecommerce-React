0235da20b1dbc221b2ffe45538789e898bc1608d
Minh Pham <buibinhminhpham2004@gmail.com>
2023-10-18 01:25:13 +0700

    Add React Router

--- admin-dashboard/src/App.js
+++ admin-dashboard/src/App.js
@@ -1,3 +1,17 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Layout from './component/Shared/Layout'
+import Dashboard from './component/Dashboard'
+import Products from './component/Products'
 export default function App() {
-    return <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Layout />}>
+                    <Route index element={<Dashboard />} />
+                    <Route path="products" element={<Products />} />
+                </Route>
+                <Route path="login" element={<div>This is Admin Login Page</div>}></Route>
+            </Routes>
+        </BrowserRouter>
+    )
 }
